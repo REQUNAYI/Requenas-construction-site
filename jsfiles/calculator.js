99a64d406561f39f2f998bcc409227bce578c3e5
requnayi <requnayi456@gmail.com>
2025-08-14 23:36:32 -0600

    beautifying pages and looks

--- jsfiles/calculator.js
+++ jsfiles/calculator.js
@@ -1,6 +1,10 @@
+//@Nayib Requena
+//@ August 18, 2025
 document.getElementById('calculate').addEventListener('click', function() {
+            
             const convertInchesToFt = parseFloat(document.getElementById('height').value) / 12;
             const getarea= parseFloat(document.getElementById('length').value) * parseFloat(document.getElementById('width').value);
             const volume = convertInchesToFt * getarea / 27 * 6
+            // below fetches and displays results
             document.getElementById('result').innerText = `Sacks needed: ${volume.toFixed(2)} Sack`;
         });
